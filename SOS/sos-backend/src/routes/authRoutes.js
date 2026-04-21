e3bc7fd71337faf71433050f4f5e480432d0234f
AKASH SARKAR <itsmeakashsarkar@gmail.com>
2026-04-21 22:14:15 +0530

    Add routes for hospital registration and login

--- SOS/sos-backend/src/routes/authRoutes.js
+++ SOS/sos-backend/src/routes/authRoutes.js
@@ -3,10 +3,12 @@ const router  = express.Router();
 const ctrl    = require('../controllers/authController');
 const { protectUser } = require('../middleware/auth');
 
+
 // ── Patient ───────────────────────────────────────────────────────────────────
 router.post('/user/register', ctrl.registerUser);
 router.post('/user/login',    ctrl.loginUser);
 router.patch('/user/location',protectUser, ctrl.updateUserLocation);
+
 
 // ── Hospital ──────────────────────────────────────────────────────────────────
 router.post('/hospital/register', ctrl.registerHospital);
